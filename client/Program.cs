71e7b49a5606e29b95899450cd7a8495167dad23
Kavin Thirukonda <kavnthir@vt.edu>
2022-11-07 18:49:59 -0500

    server client

--- client/Program.cs
+++ client/Program.cs
@@ -1,14 +1,33 @@
 ﻿using System;
+using LiteNetLib;
+using System.Threading;
 
 namespace otherworld
 {
     public static class Program
     {
-        [STAThread]
+        
         static void Main()
         {
-            using (var game = new Surface())
-                game.Run();
+            // using (var game = new Surface())
+            //    game.Run();
+
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager client = new NetManager(listener);
+            client.Start();
+            client.Connect("localhost", 7777 , "SomeConnectionKey");
+            listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) =>
+            {
+                Console.WriteLine("We got: {0}", dataReader.GetString(100 /* max length of string */));
+                dataReader.Recycle();
+            };
+
+            while (!Console.KeyAvailable) {
+                client.PollEvents();
+                Thread.Sleep(15);
+            }
+
+            client.Stop();
         }
     }
 }
---
+++ server/Server.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+namespace otherworld_server {
+    public static class Server {
+        [STAThread]
+        static void Main() {
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager server = new NetManager(listener);
+            server.Start(7777);
+
+            listener.ConnectionRequestEvent += request =>
+            {
+                if (server.ConnectedPeersCount < 10 /* max connections */)
+                    request.AcceptIfKey("SomeConnectionKey");
+                else
+                    request.Reject();
+            };
+
+            listener.PeerConnectedEvent += peer =>
+            {
+                Console.WriteLine("We got connection: {0}", peer.EndPoint); // Show peer ip
+                NetDataWriter writer = new NetDataWriter();                 // Create writer class
+                writer.Put("Hello client!");                                // Put some string
+                peer.Send(writer, DeliveryMethod.ReliableOrdered);             // Send with reliability
+            };
+
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+                Thread.Sleep(15);
+            }
+            server.Stop();
+        }
+    }
+}
