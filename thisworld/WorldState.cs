41c128075a5a0f12e1bc804b2d10a95bb0460e56
Kavin Thirukonda <kavnthir@vt.edu>
2022-11-09 23:43:10 -0500

    shared lib

---
+++ thisworld/WorldState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace thisworld {
+    public class WorldState {
+
+        private List<Player> _players;
+
+        public string Export() {
+            return "";
+        }
+    }
+}
